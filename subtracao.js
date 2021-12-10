77521e59325a04aa1a7011c9cad0d407fe1326c2
jojox6 <dougx6@gmail.com>
2021-12-10 16:38:32 -0300

    Subtração com JavaScript

---
+++ subtracao.js
@@ -0,0 +1,17 @@
+/**
+ * Subtraia um número de outro com JavaScript
+Também podemos subtrair um número de outro.
+
+JavaScript usa o -símbolo para subtração.
+
+Exemplo
+
+const myVar = 12 - 6;
+myVarteria o valor 6.
+
+Mude o então a diferença é 12.
+
+
+ */
+
+const difference = 45 - 33;
