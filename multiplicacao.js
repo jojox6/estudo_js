111440c17d5209a3a6559ab2b9e3c0ab895f25d0
jojox6 <dougx6@gmail.com>
2021-12-10 16:40:29 -0300

    Multiplicação em JavaScript

---
+++ multiplicacao.js
@@ -0,0 +1,17 @@
+/**
+ * Multiplique dois números com JavaScript
+Também podemos multiplicar um número por outro.
+
+JavaScript usa o *símbolo para multiplicação de dois números.
+
+Exemplo
+
+const myVar = 13 * 13;
+myVarteria o valor 169.
+
+Altere o para que o produto seja igual 80.
+
+
+ */
+
+const product = 8 * 10;
