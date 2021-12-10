ce22ee64d233b03cb359b15742d8321fe08514a3
jojox6 <dougx6@gmail.com>
2021-12-10 16:42:29 -0300

    Divisão com JavaScript

---
+++ divisao.js
@@ -0,0 +1,17 @@
+/**
+ * Divida um número por outro com JavaScript
+Também podemos dividir um número por outro.
+
+JavaScript usa o /símbolo de divisão.
+
+Exemplo
+
+const myVar = 16 / 2;
+myVaragora tem o valor 8.
+
+Altere o 0para que quotientseja igual a 2.
+
+
+ */
+
+const quotient = 66 / 33;
