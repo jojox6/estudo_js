2d211e53398f440307e15a444e58c21d66cb7fb2
jojox6 <dougx6@gmail.com>
2021-12-10 16:49:32 -0300

    Números decimais em js = Float

---
+++ decimal.js
@@ -0,0 +1,15 @@
+/**
+ * Crie números decimais com JavaScript
+Podemos armazenar números decimais em variáveis ​​também. Os números decimais às vezes são chamados de números de ponto flutuante ou flutuantes .
+
+Nota: Nem todos os números reais podem ser representados com precisão em ponto flutuante . Isso pode levar a erros de arredondamento. Detalhes aqui .
+
+Crie uma variável myDecimale dê a ela um valor decimal com uma parte fracionária (por exemplo 5.7).
+
+
+ */
+
+const ourDecimal = 5.7;
+
+// Only change code below this line
+const myDecimal = 5.7;
