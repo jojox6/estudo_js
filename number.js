e6dcfa78890ab1d8e6edceefcee5ac678b578f38
jojox6 <dougx6@gmail.com>
2021-12-10 16:35:45 -0300

    number com soba em javascript

---
+++ number.js
@@ -0,0 +1,17 @@
+/**
+ * Adicionar dois números com JavaScript
+Number é um tipo de dados em JavaScript que representa dados numéricos.
+
+Agora vamos tentar adicionar dois números usando JavaScript.
+
+JavaScript usa o +símbolo como um operador de adição quando colocado entre dois números.
+
+Exemplo:
+
+const myVar = 5 + 10;
+myVaragora tem o valor 15.
+
+Altere o 0para que a soma seja igual 20.
+ */
+
+const sum = 10 + 10;
