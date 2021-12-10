02e39bc7dfd6194e6d9c3fe7558b10d4e84abbb8
jojox6 <dougx6@gmail.com>
2021-12-10 16:46:56 -0300

    Decrementar -1 com js = i--; ou var = var - 1;

---
+++ Decrementar.js
@@ -0,0 +1,19 @@
+/**
+ * Decremente um número com JavaScript
+Você pode facilmente diminuir ou diminuir uma variável em um com o --operador.
+
+i--;
+é o equivalente a
+
+i = i - 1;
+Nota: A linha inteira se transforma i--;, eliminando a necessidade do sinal de igual.
+
+Altere o código para usar a --operadora myVar.
+
+
+ */
+
+let myVar = 11;
+
+// Only change code below this line
+myVar--;
