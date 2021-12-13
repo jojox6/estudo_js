5f06e0309f391ecc0024c44cd35e19819e5793f4
jojox6 <dougx6@gmail.com>
2021-12-13 18:49:45 -0300

    Concatenando String

---
+++ concatenandoStrings.js
@@ -0,0 +1,21 @@
+/*
+Concatenando Strings com Operador Plus
+Em JavaScript, quando o +operador é usado com um Stringvalor, ele é chamado de operador de concatenação . Você pode construir uma nova string a partir de outras strings concatenando -as.
+
+Exemplo
+
+'My name is Alan,' + ' I concatenate.'
+Nota: Cuidado com os espaços. A concatenação não adiciona espaços entre strings concatenadas, portanto, você mesmo precisará adicioná-los.
+
+Exemplo:
+
+const ourStr = "I come first. " + "I come second.";
+A string I come first. I come second.seria exibida no console.
+
+Construa a myStrpartir das strings This is the start.e This is the end.usando o +operador. Certifique-se de incluir um espaço entre as duas strings.
+
+
+ */
+
+const myStr = "This is the start. " + "This is the end."; // Change this line
+console.log(myStr);
