5946313154b94d9f3a35dbbfd8987d5b117b3e8c
jojox6 <dougx6@gmail.com>
2021-12-13 18:04:13 -0300

    Subtração Composta.

---
+++ subtracaoComposta.js
@@ -0,0 +1,28 @@
+/**
+ * Atribuição composta com subtração aumentada
+Como o +=operador, -=subtrai um número de uma variável.
+
+myVar = myVar - 5;
+irá subtrair 5de myVar. Isso pode ser reescrito como:
+
+myVar -= 5;
+Converter as atribuições para a, be cusar o -=operador.
+
+
+ */
+
+let a = 11;
+let b = 9;
+let c = 3;
+
+/* Only change code below this line
+a = a - 6;
+b = b - 15;
+c = c - 1;*/
+
+//depois
+a -= 6;
+b -= 15;
+c -= 1;
+//By: Joseph
+console.log(a,b,c);
