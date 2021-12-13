6d8c2d02819eaa29e4ff0139e6e521944bada941
jojox6 <dougx6@gmail.com>
2021-12-13 16:30:19 -0300

    Divide Números décimais em js.

---
+++ divideDecimal.js
@@ -0,0 +1,12 @@
+/**
+ * Divida um decimal por outro com JavaScript
+Agora vamos dividir um decimal por outro.
+
+Altere o 0.0para que quotientseja igual a 2.2.
+ */
+
+//antes
+const quotient = 0.0 / 2.0;
+
+//depois
+const quotient = 4.4 / 2.0; // Change this line
