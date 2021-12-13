6669a069b7a93bd28dc14908b314f71cef7bf97b
jojox6 <dougx6@gmail.com>
2021-12-13 16:48:55 -0300

    Encontrando um Restante em JavaScript.

---
+++ encontrandoRestaurante.js
@@ -0,0 +1,31 @@
+/**
+ * Encontrando um Restante em JavaScript
+O operador restante% fornece o restante da divisão de dois números.
+
+Exemplo
+
+5% 2 = 1 porque
+Math.floor (5/2) = 2 (Quociente)
+2 * 2 = 4
+5 - 4 = 1 (Restante)
+Uso
+Em matemática, um número pode ser verificado para ser par ou ímpar verificando o restante da divisão do número por 2.
+
+17% 2 = 1 (17 é ímpar)
+48% 2 = 0 (48 é par )
+Nota: O operador restante às vezes é denominado incorretamente como o operador módulo. É muito semelhante ao módulo, mas não funciona corretamente com números negativos.
+
+Defina remainderigual ao resto de 11dividido 3usando o operador resto ( %).
+
+
+ */
+
+//antes
+//const remainder = 0;
+
+//depois
+
+const remainder = 11 % 3;
+//Mosntrado o resto da divisão no console by: Joseph
+
+console.log(remainder);
