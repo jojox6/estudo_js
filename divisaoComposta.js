5ac3622d086fe1e88a2cd2a582dbaf9d797633d2
jojox6 <dougx6@gmail.com>
2021-12-13 18:13:43 -0300

    Divisão Composta.

---
+++ divisaoComposta.js
@@ -0,0 +1,23 @@
+/*
+Atribuição composta com divisão aumentada
+O /=operador divide uma variável por outro número.
+
+myVar = myVar / 5;
+Irá dividir myVarpor 5. Isso pode ser reescrito como:
+
+myVar /= 5;
+Converter as atribuições para a, be cusar o /=operador.
+
+
+ */
+
+let a = 48;
+let b = 108;
+let c = 33;
+
+// Only change code below this line
+a /= 12;
+b /= 4;
+c /= 11;
+
+console.log(a,b,c);
