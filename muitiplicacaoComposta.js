aaf2bb071cc6cdb217f4d274f54ac44a2b87c52f
jojox6 <dougx6@gmail.com>
2021-12-13 18:10:16 -0300

    Multiplicação Composta.

---
+++ muitiplicacaoComposta.js
@@ -0,0 +1,19 @@
+/*Atribuição composta com multiplicação aumentada
+O *=operador multiplica uma variável por um número.
+
+myVar = myVar * 5;
+irá multiplicar myVarpor 5. Isso pode ser reescrito como:
+
+myVar *= 5;
+Converter as atribuições para a, be cusar o *=operador. */
+
+let a = 5;
+let b = 12;
+let c = 4.6;
+
+// Only change code below this line
+a *= 5;
+b *= 3;
+c *= 10;
+
+console.log(a,b,c);
