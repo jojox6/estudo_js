f01587106fff569d22968db766e019f4c6094818
jojox6 <dougx6@gmail.com>
2021-12-13 16:23:10 -0300

    Multiplicaçẽo de números Decimais.

---
+++ multiplicaDecimais.js
@@ -0,0 +1,13 @@
+/**
+ * Multiplique dois decimais com JavaScript
+Em JavaScript, você também pode realizar cálculos com números decimais, assim como números inteiros.
+
+Vamos multiplicar duas casas decimais para obter o produto.
+
+Altere o 0.0para que o produto seja igual 5.0.
+ */
+//antes
+const product = 2.0 * 0.0;
+
+//depois
+const product = 2.0 * 2.5;
