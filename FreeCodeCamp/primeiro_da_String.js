a379ccf7794788bf209b5aa1027c3e190f712a10
jojox6 <dougx6@gmail.com>
2021-12-14 18:49:09 -0300

    Notação de colchetes.

---
+++ FreeCodeCamp/primeiro_da_String.js
@@ -0,0 +1,25 @@
+/*
+Use a notação de colchetes para encontrar o primeiro caractere em uma string
+A notação de colchetes é uma maneira de obter um caractere em um índice específico dentro de uma string.
+
+A maioria das linguagens de programação modernas, como JavaScript, não começa a contar em 1 como os humanos fazem. Eles começam em 0. Isso é conhecido como indexação baseada em zero .
+
+Por exemplo, o caractere no índice 0 da palavra Charlesé C. Portanto const firstName = "Charles", se , você pode obter o valor da primeira letra da string usando firstName[0].
+
+Exemplo:
+
+const firstName = "Charles";
+const firstLetter = firstName[0];
+firstLetterteria um valor da string C.
+
+Use a notação de colchetes para encontrar o primeiro caractere na lastNamevariável e atribuí-lo a firstLetterOfLastName.
+
+Dica: tente olhar o exemplo acima se tiver dúvidas.
+*/
+
+// Setup
+let firstLetterOfLastName = "";
+const lastName = "Lovelace";
+
+// Only change code below this line
+console.log(firstLetterOfLastName = lastName[0]); // Change this line
