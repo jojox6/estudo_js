36850fe42919c209436e862e093788e98575dfac
jojox6 <dougx6@gmail.com>
2021-12-14 18:35:36 -0300

    Encontre o comprimento de uma palavra

---
+++ FreeCodeCamp/econtreComprimento.js
@@ -0,0 +1,21 @@
+/*
+Encontre o comprimento de uma corda
+Você pode encontrar o comprimento de um Stringvalor escrevendo .lengthapós a variável de string ou literal de string.
+
+console.log("Alan Peter".length);
+O valor 10seria exibido no console.
+
+Por exemplo, se criarmos uma variável const firstName = "Ada", poderíamos descobrir o comprimento da string Adausando a firstName.lengthpropriedade.
+
+Use a .lengthpropriedade para contar o número de caracteres na lastNamevariável e atribuí-los lastNameLength.
+
+
+*/
+
+// Setup
+let lastNameLength = 0;
+const lastName = "Lovelace";
+
+
+// Only change code below this line
+console.log(lastNameLength += lastName.length);
