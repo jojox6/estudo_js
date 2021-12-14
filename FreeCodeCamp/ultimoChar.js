5f3a16e79bf12c276e79e8318793d4dbc9cf74e1
jojox6 <dougx6@gmail.com>
2021-12-14 19:00:03 -0300

    encontrar o último caractere em uma string

---
+++ FreeCodeCamp/ultimoChar.js
@@ -0,0 +1,26 @@
+/*
+Use a notação de colchetes para encontrar o último caractere em uma string
+Para obter a última letra de uma string, você pode subtrair uma do comprimento da string.
+
+Por exemplo, se const firstName = "Ada", você pode obter o valor da última letra da string usando firstName[firstName.length - 1].
+
+Exemplo:
+
+const firstName = "Ada";
+const lastLetter = firstName[firstName.length - 1];
+lastLetterteria um valor da string a.
+
+Use a notação de colchetes para encontrar o último caractere na lastNamevariável.
+
+Dica: tente olhar o exemplo acima se tiver dúvidas.
+
+
+*/
+
+// Setup
+const lastName = "Lovelace";
+
+// Only change code below this line
+const lastLetterOfLastName = lastName[lastName.length - 1]; // Change this line
+
+console.log(lastLetterOfLastName);
