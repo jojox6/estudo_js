3bd1d8dc7200922a3f9d6518b84e7e500655bf99
jojox6 <dougx6@gmail.com>
2021-12-14 18:56:39 -0300

    Use a notação de colchetes para encontrar o enésimo caractere em uma string

---
+++ FreeCodeCamp/enesimoChar.js
@@ -0,0 +1,24 @@
+/*
+Use a notação de colchetes para encontrar o enésimo caractere em uma string
+Você também pode usar a notação de colchetes para obter o caractere em outras posições dentro de uma string.
+
+Lembre-se de que os computadores começam a contar em 0, então o primeiro caractere é na verdade o caractere zero.
+
+Exemplo:
+
+const firstName = "Ada";
+const secondLetterOfFirstName = firstName[1];
+secondLetterOfFirstNameteria um valor da string d.
+
+Vamos tentar thirdLetterOfLastNameigualar a terceira letra da lastNamevariável usando a notação de colchetes.
+
+Dica: tente olhar o exemplo acima se tiver dúvidas.
+
+
+*/
+
+// Setup
+const lastName = "Lovelace";
+
+// Only change code below this line
+const thirdLetterOfLastName = lastName[2]; // Change this line
