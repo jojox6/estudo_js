cebf85220364f19ef500d1a34f6d39c9a396097d
jojox6 <dougx6@gmail.com>
2021-12-14 19:03:24 -0300

    encontrar o enésimo ao último caractere em uma string

---
+++ FreeCodeCamp/enesimo_e_ultimo.js
@@ -0,0 +1,24 @@
+/*
+Use a notação de colchetes para encontrar o enésimo ao último caractere em uma string
+Você pode usar o mesmo princípio que acabamos de usar para recuperar o último caractere em uma string para recuperar o enésimo ao último caractere.
+
+Por exemplo, você pode obter o valor da terceira à última letra da const firstName = "Augusta"string usandofirstName[firstName.length - 3]
+
+Exemplo:
+
+const firstName = "Augusta";
+const thirdToLastLetter = firstName[firstName.length - 3];
+thirdToLastLetterteria um valor da string s.
+
+Use a notação de colchetes para encontrar o penúltimo caractere na lastNamestring.
+
+Dica: tente olhar o exemplo acima se tiver dúvidas.
+
+
+*/
+
+// Setup
+const lastName = "Lovelace";
+
+// Only change code below this line
+const secondToLastLetterOfLastName = lastName[lastName.length - 2]; // Change this line
