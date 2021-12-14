d31f5864ce8ab3db9b73230c912afcc8b0eea015
jojox6 <dougx6@gmail.com>
2021-12-14 14:23:29 -0300

    Sintaxe Básica

---
+++ Doc_MDN/Sintaxe básica/sintaxeBasica.js
@@ -0,0 +1,14 @@
+/*
+JavaScript pega emprestado a maior parte de sua sintaxe do Java, mas também é influenciado por Awk, Perl e Python.
+
+JavaScript é case-sensitive e usa o conjunto de caracteres Unicode. Por exemplo, a palavra Früh (que significa "cedo" em Alemão) pode ser usada como nome de variável.
+
+Mas a variável früh não é a mesma que Früh porque JavaScript é case sensitive.
+
+No JavaScript, instruções são chamadas de declaração e são separadas por um ponto e vírgula (;). Espaços, tabulação e uma nova linha são chamados de espaços em branco. O código fonte dos scripts em JavaScript são lidos da esquerda para a direita e são convertidos em uma sequência de elementos de entrada como simbolos, caracteres de controle, terminadores de linha, comentários ou espaço em branco. ECMAScript também define determinadas palavras-chave e literais, e tem regras para inserção automática de ponto e vírgula (ASI) para terminar as declarações. No entanto, recomenda-se sempre adicionar ponto e vírgula no final de suas declarações; isso evitará alguns imprevistos. Para obter mais informações, consulte a referência detalhada sobre a gramática léxica do JavaScript.
+
+
+ */
+
+var Früh = "foobar";
+console.log(Früh);
