7aba0039a1bcb01385d67bf6042fd95d28165bbe
jojox6 <dougx6@gmail.com>
2021-12-14 17:34:15 -0300

    Construindo Strings com Variáveis

---
+++ FreeCodeCamp/stringsComVar.js
@@ -0,0 +1,20 @@
+/*
+Construindo Strings com Variáveis
+Às vezes, você precisará construir uma string, no estilo Mad Libs . Usando o operador de concatenação ( +), você pode inserir uma ou mais variáveis ​​em uma string que está construindo.
+
+Exemplo:
+
+const ourName = "freeCodeCamp";
+const ourStr = "Hello, our name is " + ourName + ", how are you?";
+ourStrteria um valor da string Hello, our name is freeCodeCamp, how are you?.
+
+Defina myName como uma string igual ao seu nome e crie myStrcom myNameentre as strings My name is e and I am well!
+
+
+
+*/
+
+// Only change code below this line
+const myName = "Joseph";
+const myStr = "My name is " + myName + " and I am well!";
+console.log(myStr);
