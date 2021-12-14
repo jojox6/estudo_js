fce8036d2834bc3273e40e380768edcc440aca8f
jojox6 <dougx6@gmail.com>
2021-12-14 15:01:43 -0300

    Variáveis Globais.

---
+++ Doc_MDN/Variáveis Globais/varGlobal.js
@@ -0,0 +1,5 @@
+/*
+Variáveis globais são propriedades do objeto global. Em páginas web o objeto global é a window, assim você pode configurar e acessar variáveis globais utilizando a sintaxe window.variavel. 
+
+Consequentemente, você pode acessar variáveis globais declaradas em uma janela ou frame ou frame de outra janela. Por exemplo, se uma variável chamada phoneNumber é declarada em um documento, você pode consultar esta variável de um frame como parent.phoneNumber.
+*/
