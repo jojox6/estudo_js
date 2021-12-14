ab16b589a5b4a7d84a1f123a90a01e3248592755
jojox6 <dougx6@gmail.com>
2021-12-14 19:21:36 -0300

    Arrays

---
+++ FreeCodeCamp/matrizes.js
@@ -0,0 +1,11 @@
+/*
+Armazene vários valores em uma variável usando matrizes JavaScript
+Com arrayvariáveis JavaScript , podemos armazenar vários dados em um só lugar.
+
+Você inicia uma declaração de matriz com um colchete de abertura, termina com um colchete de fechamento e coloca uma vírgula entre cada entrada, assim:
+
+const sandwich = ["peanut butter", "jelly", "bread"];
+Modifique a nova matriz myArraypara que contenha uma string e um número (nessa ordem).
+*/// Only change code below this line
+const myArray = ["Joseph", 26];
+
