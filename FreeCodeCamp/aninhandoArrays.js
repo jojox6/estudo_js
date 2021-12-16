2c4fcc376232a39d887666aba6a9b77d18654c08
jojox6 <dougx6@gmail.com>
2021-12-16 14:53:30 -0300

    Arrays aninhados.

---
+++ FreeCodeCamp/aninhandoArrays.js
@@ -0,0 +1,12 @@
+/*
+Aninhar uma matriz dentro de outra matriz
+Você também pode aninhar matrizes dentro de outras matrizes, como a seguir:
+
+const teams = [["Bulls", 23], ["White Sox", 45]];
+Isso também é chamado de array multidimensional .
+
+Crie uma matriz aninhada chamada myArray.
+*/
+
+const myArray = [["Joseph", 26], ["Vanessa", 25]];
+console.log(myArray);
