afee8cc80c7b1104895ed0b5d28691512aa1b11c
jojox6 <dougx6@gmail.com>
2021-12-16 16:47:30 -0300

    Lista de compras com Arrays aninhados.

---
+++ FreeCodeCamp/lista_de_Compas.js
@@ -0,0 +1,19 @@
+/*
+Lista de compras
+Crie uma lista de compras na variável myList. A lista deve ser uma matriz multidimensional contendo várias submatrizes.
+
+O primeiro elemento em cada submatriz deve conter uma string com o nome do item. O segundo elemento deve ser um número que representa a quantidade, ou seja,
+
+["Chocolate Bar", 15]
+Deve haver pelo menos 5 submatrizes na lista.
+
+*/
+
+const myList = [
+    ["Caixa de Chocolate", 11.90],
+    ["Salgado", 1.99],
+    ["Refrigerante", 7.99],
+    ["Sal", 1],
+    ["Açúcar", 3.89]
+];
+console.log(myList);
