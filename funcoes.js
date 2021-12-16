7d86ac8fda722ccd3cd762d9b6a1663b9a3403c1
jojox6 <dougx6@gmail.com>
2021-12-16 16:51:15 -0300

    Aprendendo a reutilizar código com funções.

---
+++ funcoes.js
@@ -0,0 +1,20 @@
+/*
+Escreva JavaScript reutilizável com funções
+Em JavaScript, podemos dividir nosso código em partes reutilizáveis ​​chamadas funções .
+
+Aqui está um exemplo de função:
+
+function functionName() {
+  console.log("Hello World");
+}
+Você pode chamar ou invocar esta função usando seu nome seguido por parênteses, como este: functionName();Cada vez que a função é chamada, ela imprimirá a mensagem Hello Worldno console dev. Todo o código entre as chaves será executado toda vez que a função for chamada.
+
+Crie uma função chamada reusableFunctionque imprima a string Hi Worldno console dev.
+Chame a função.
+
+*/
+
+function reusableFunction(){
+    console.log("Hi World");
+}
+reusableFunction();
