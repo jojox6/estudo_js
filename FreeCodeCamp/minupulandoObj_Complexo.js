692d298e7a26b0d85e647fef46291922ff38e80a
jojox6 <dougx6@gmail.com>
2022-01-03 16:08:26 -0300

    Manipulando objetos complexos

---
+++ FreeCodeCamp/minupulandoObj_Complexo.js
@@ -0,0 +1,22 @@
+// Manipulando objetos complexos
+
+
+
+
+var myMusic = [
+    {
+      artist: "Billy Joel",
+      title: "Piano Man",
+      release_year: 1973,
+      formats: ["CD", "8T", "LP"],
+      gold: true
+    },
+    // Add record here
+    {
+      artist: "Deep Purple",
+      title: "Smoke on the water",
+      release_year: 1976,
+      formats: ["CD", "8T", "LP"]
+    }
+  ];
+console.log(myMusic);
