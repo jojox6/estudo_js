812c7639b59bb388747ead8b75f6db3627bcb671
jojox6 <dougx6@gmail.com>
2022-01-12 14:15:08 -0300

    for

--- JS_ProgramadorBR/script.js
+++ JS_ProgramadorBR/script.js
@@ -1,7 +1,22 @@
-var idade = 17;
+// Forma 1 de Criar um Array
+/*
+var vetor = new Array();
 
-if(idade >= 18){
-    console.log("Pode!");
-}else{
-    console.log("Não pode!");
+
+var alunos = new Array("Joseph", "Vanessa", "Jade", "Jeane");
+console.log(alunos[3]);
+*/
+//Forma 2 
+
+var alunos = ["Joseph", "Jade", "Jeane", "Vanessa", "Marco"];
+
+// Forma 1
+/*for (var i = 0; i < alunos.length; i++){
+    console.log(alunos[i]);
+}*/
+
+// Forma 2
+
+for (var aluno of alunos){
+    console.log(aluno);
 }
