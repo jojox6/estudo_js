03878a17e2906c67a0bc054633530b72f4bfdf48
jojox6 <dougx6@gmail.com>
2022-01-12 14:34:57 -0300

    Funções

--- JS_ProgramadorBR/script.js
+++ JS_ProgramadorBR/script.js
@@ -1,22 +1,7 @@
-// Forma 1 de Criar um Array
-/*
-var vetor = new Array();
-
-
-var alunos = new Array("Joseph", "Vanessa", "Jade", "Jeane");
-console.log(alunos[3]);
-*/
-//Forma 2 
-
-var alunos = ["Joseph", "Jade", "Jeane", "Vanessa", "Marco"];
-
-// Forma 1
-/*for (var i = 0; i < alunos.length; i++){
-    console.log(alunos[i]);
-}*/
-
-// Forma 2
-
-for (var aluno of alunos){
-    console.log(aluno);
+function media(nota1,nota2){
+    let media;
+    media = (nota1+nota2)/2;
+    return media;
 }
+
+console.log(media(5,9));
